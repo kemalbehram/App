e297c28be03a1c7eb2d16c41009262254dd9d4ba
bubbafinance <87012372+bubbafinance@users.noreply.github.com>
2021-08-15 10:34:10 +0300

    Add files via upload

---
+++ src/front/config/mainnet/bep20.js
@@ -0,0 +1,20 @@
+export default {
+    swap: {
+        address: '0x92648e4537cdfa1ee743a244465a31aa034b1ce8',
+        decimals: 0,
+        fullName: 'SWAP',
+        canSwap: true,
+    },
+    btcb: {
+        address: '0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c',
+        decimals: 18,
+        fullName: 'BTCB Token',
+        canSwap: true,
+    },
+    BUBBA: {
+        address: '0x7D2441A94E171EBEaF5CD7A1ebEb2734629dD7e2',
+        decimals: 9,
+        fullName: 'BUBBA Token',
+        canSwap: true,
+    }
+}
