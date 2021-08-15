41ebadb5c4c46a6af33f28061422dce76d9271ba
bubbafinance <87012372+bubbafinance@users.noreply.github.com>
2021-08-15 10:42:18 +0300

    Add files via upload

---
+++ src/front/shared/components/ui/CloseIcon/CloseIcon.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import cssModules from 'react-css-modules'
+import styles from './CloseIcon.scss'
+
+type ComponentProps = {
+  onClick: (...args: any) => void
+  styleName: string
+  id?: string
+}
+
+const CloseIcon = (props: ComponentProps) => {
+  const { onClick, styleName: externalName, id = '', ...rest } = props
+
+  return (
+    <div id={id} styleName="button" {...rest} role="closeButton" onClick={onClick}>
+      <div styleName="icon" role="closeIcon" />
+    </div>
+  )
+}
+
+export default cssModules(CloseIcon, styles, { allowMultiple: true })
